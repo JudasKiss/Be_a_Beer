915052387b3470daf4e2dc1889c72143193febac
jongwook100 <55087000+jongwook100@users.noreply.github.com>
2021-03-16 17:17:29 +0900

    ui v2
    ui v2

--- app/src/main/java/com/cookandroid/beer/HistoryActivity.java
+++ app/src/main/java/com/cookandroid/beer/HistoryActivity.java
@@ -1,19 +1,55 @@
 package com.cookandroid.beer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class HistoryActivity extends AppCompatActivity {
-
+    ListView listview = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        ListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listView1);
+        listview.setAdapter(adapter);
+
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.beer),
+                "카스", "5%");
+
+
+        EditText editTextFilter = (EditText)findViewById(R.id.editTextFilter);
+        editTextFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable edit) {
+                String filterText = edit.toString() ;
+                ((ListViewAdapter)listview.getAdapter()).getFilter().filter(filterText) ;
+            }
+        });
         ImageButton button1 = findViewById(R.id.imageButton);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +66,6 @@ public class HistoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/cookandroid/beer/RankingActivity.java
+++ app/src/main/java/com/cookandroid/beer/RankingActivity.java
@@ -1,18 +1,54 @@
 package com.cookandroid.beer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class RankingActivity extends AppCompatActivity {
-
+    ListView listview = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+        ListViewAdapter adapter;
+        // Adapter 생성
+        adapter = new ListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listView1);
+        listview.setAdapter(adapter);
+
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.beer),
+                "카스", "5%");
+
+
+        EditText editTextFilter = (EditText)findViewById(R.id.editTextFilter);
+        editTextFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable edit) {
+                String filterText = edit.toString() ;
+                ((ListViewAdapter)listview.getAdapter()).getFilter().filter(filterText) ;
+            }
+        });
+
         ImageButton button1 = findViewById(R.id.imageButton);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
