48ac9b17a690c1fcfd90a87d67a2f30fb395369d
Frank <aa6618@mju.ac.kr>
2021-03-21 05:10:29 +0900

    Barcode added

---
+++ app/src/main/java/com/cookandroid/beer/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.cookandroid.beer;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/cookandroid/beer/HistoryActivity.java
+++ app/src/main/java/com/cookandroid/beer/HistoryActivity.java
@@ -1,8 +1,10 @@
 package com.cookandroid.beer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
@@ -12,8 +14,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class HistoryActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class HistoryActivity extends AppCompatActivity implements View.OnClickListener{
     ListView listview = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +72,53 @@ public class HistoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ImageButton button3 = findViewById(R.id.imageButton3);
+        button3.setOnClickListener(this);
 
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        scanCode();
+    }
+
+    private void scanCode(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scanning Code");
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents() != null){
+                AlertDialog.Builder builder =new AlertDialog.Builder(this);
+                builder.setMessage(result.getContents());
+                builder.setTitle("Scanning Result");
+                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        scanCode();
+                    }
+                }).setNegativeButton("finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            else{
+                Toast.makeText(this, "No Results", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
