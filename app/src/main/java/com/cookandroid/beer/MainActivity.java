d037cb1e5e5b8447d02a141849988ddf46217d0c
sbkim0309 <sbkim0309@naver.com>
2021-03-21 01:32:41 +0900

    UI update

--- app/src/main/java/com/cookandroid/beer/MainActivity.java
+++ app/src/main/java/com/cookandroid/beer/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),RankingActivity.class);
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
                 startActivity(intent);
             }
         });
