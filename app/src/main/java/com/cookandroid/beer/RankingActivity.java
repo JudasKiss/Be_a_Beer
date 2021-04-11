be6cb7318e915b266b63d2e05dff54e8594d584e
jongwook100 <55087000+jongwook100@users.noreply.github.com>
2021-04-11 17:09:39 +0900

    ui 하단바
    ui 하단바

--- app/src/main/java/com/cookandroid/beer/RankingActivity.java
+++ app/src/main/java/com/cookandroid/beer/RankingActivity.java
@@ -1,20 +1,20 @@
 package com.cookandroid.beer;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -24,6 +24,19 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+        Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DBexample.class);
+                startActivity(intent);
+            }
+        });
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            startMainActivity();
+        }
+
+        ListViewAdapter adapter;
 
         //Initialize And Assign Variable
         BottomNavigationView bottomNabvigationView = findViewById(R.id.bottom_navigation);
@@ -33,18 +46,18 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
-                    case R.id.ranking:
-                        startActivity(new Intent(getApplicationContext(),RankingActivity.class));
-                        overridePendingTransition(0,0);
-                        Toast.makeText(getApplicationContext(),"랭킹 탭 선택됨",Toast.LENGTH_LONG).show();
-                        return true;
                     case R.id.mainbeer:
-                        startActivity(new Intent(getApplicationContext(),MainbeerActivity.class));
+                        startActivity(new Intent(getApplicationContext(), MainbeerActivity.class));
                         overridePendingTransition(0,0);
                         Toast.makeText(getApplicationContext(),"메인 탭 선택됨",Toast.LENGTH_LONG).show();
                         return true;
+                    case R.id.ranking:
+                        startActivity(new Intent(getApplicationContext(), RankingActivity.class));
+                        overridePendingTransition(0,0);
+                        Toast.makeText(getApplicationContext(),"랭킹 탭 선택됨",Toast.LENGTH_LONG).show();
+                        return true;
                     case R.id.myinfo:
-                        startActivity(new Intent(getApplicationContext(),MyInfo.class));
+                        startActivity(new Intent(getApplicationContext(), MyInfo.class));
                         overridePendingTransition(0,0);
                         Toast.makeText(getApplicationContext(),"내정보 탭 선택됨",Toast.LENGTH_LONG).show();
                         return true;
@@ -52,8 +65,6 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
                 return false;
             }
         });
-
-        ListViewAdapter adapter;
 
         // Adapter 생성
         adapter = new ListViewAdapter();
@@ -71,9 +82,20 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
         ImageButton button3 = findViewById(R.id.imageButton3);
         button3.setOnClickListener(this);
 
+        findViewById(R.id.logout).setOnClickListener(onClickListener);
+
     }
-
-
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.logout:
+                    FirebaseAuth.getInstance().signOut();
+                    startMainActivity();
+                    break;
+            }
+        }
+    };
 
 
     @Override
@@ -95,8 +117,12 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null){
             if(result.getContents() != null){
-                AlertDialog.Builder builder =new AlertDialog.Builder(this);
-                builder.setMessage(result.getContents());
+                String barcode = result.getContents();
+                startRecommendActivity(barcode);
+                /*AlertDialog.Builder builder =new AlertDialog.Builder(this);
+                String url = "https://www.wine21.com/13_search/beer_view.html?Idx=";
+                String url1 = url.concat(barcode);
+                builder.setMessage(url1);
                 builder.setTitle("Scanning Result");
                 builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
                     @Override
@@ -110,7 +136,7 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
                     }
                 });
                 AlertDialog dialog = builder.create();
-                dialog.show();
+                dialog.show();*/
             }
             else{
                 Toast.makeText(this, "No Results", Toast.LENGTH_LONG).show();
@@ -118,5 +144,18 @@ public class RankingActivity extends AppCompatActivity implements View.OnClickLi
         }else{
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void startRecommendActivity(String barcode){
+        Intent intent = new Intent(this, RecommendBeer.class);
+        intent.putExtra("barcode", barcode);
+        //intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
