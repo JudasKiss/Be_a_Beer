6f979fc541144bcda4ac10287670a14bd1e6e94e
Sangwoo Kim <43949061+Sangwoo97@users.noreply.github.com>
2021-05-10 14:01:16 +0900

    DB없는 바코드 찍을시 오류팝업창 생성

--- app/src/main/java/com/cookandroid/beer/RecommendBeer.java
+++ app/src/main/java/com/cookandroid/beer/RecommendBeer.java
@@ -1,7 +1,9 @@
 package com.cookandroid.beer;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -212,25 +215,25 @@ public class RecommendBeer extends AppCompatActivity {
         if(result != null){
             if(result.getContents() != null){
                 String barcode = result.getContents();
-                startRecommendActivity(barcode);
-                /*AlertDialog.Builder builder =new AlertDialog.Builder(this);
-                String url = "https://www.wine21.com/13_search/beer_view.html?Idx=";
-                String url1 = url.concat(barcode);
-                builder.setMessage(url1);
-                builder.setTitle("Scanning Result");
-                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
+                String b = "Beer/";
+                String temp = b.concat(barcode);
+                rDatabase = FirebaseDatabase.getInstance().getReference(temp);
+                rDatabase.addValueEventListener(new ValueEventListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        scanCode();
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        try{
+                            String value = dataSnapshot.getValue().toString();
+                            startRecommendActivity(barcode);
+                        }catch(NullPointerException e){
+                            showDialog();
+                        }
                     }
-                }).setNegativeButton("finish", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("Database", "Failed to read value.", error.toException());
                     }
                 });
-                AlertDialog dialog = builder.create();
-                dialog.show();*/
             }
             else{
                 Toast.makeText(this, "No Results", Toast.LENGTH_LONG).show();
@@ -249,5 +252,29 @@ public class RecommendBeer extends AppCompatActivity {
         intent.putExtra("barcode", barcode);
         //intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    private void startDBexampleActivity(){
+        Intent intent = new Intent(this, DBexample.class);
+        intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder builder =new AlertDialog.Builder(this);
+        builder.setMessage("저희 데이터에 없는 맥주네요ㅠㅠ 다른 사용자를 위해 맥주를 추가해주시겠어요?");
+        builder.setTitle("죄송해요!");
+        builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        }).setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                startDBexampleActivity();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
